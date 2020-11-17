274ed38824a430c8fb40dd85dd5f18f0b77b3600
Cory Pilat <cpilat97@gmail.com>
2020-11-17 14:17:28 -0500

    script update

--- data_Rollup.R
+++ data_Rollup.R
@@ -19,7 +19,13 @@ library(magrittr)
 awards <- read_csv("awards.csv") %>% 
   #don't need elector info for this, getting rid of to reduce VARS
   select(., -c(AssemblyDistrict, CongressionalDistrict, StateSenatorialDistrict,
-               AssemblyDistrict_1, CongressionalDistrict_1, StateSenatorialDistrict_1))
+               AssemblyDistrict_1, CongressionalDistrict_1, StateSenatorialDistrict_1,
+               siteIdExists, nonAward, anyAward, naward, `Current HS Delegate`, 
+               `COL Seats Awarded`, cc_seats_awarded, hs4_seats_awarded,
+               hs3_seats_awarded,	cc_seats_awarded,	cc4_seats_awarded,
+               cc3_seats_awarded,	universal_seats_awarded,	hs_tots,	cc_tots,
+               cc_infants, `Base RFP` )) %>% 
+  filter((`Award Flag` == "Award" | `Award Flag` == "Award COL") & baseRFP == "B")
 
 #11/17: operating under following assumptions: 
 #   1. ONLY looking at EDY/SDY 3's & 4's. No HS, no dual awarded (HS/B5), no I/T
@@ -46,9 +52,31 @@ allSites %<>%
   mutate(.,
          total_SDY = rowSums(select(., current_SDY_3s, current_SDY_4s), na.rm = TRUE),
          total_EDY = rowSums(select(., current_EDY_3s, current_EDY_4s), na.rm = TRUE),
-         pgm_Type = case_when(total_SDY > 0 & total_EDY > 0 ~ "edySDY",
+         current_Pgm_Model = case_when(total_SDY > 0 & total_EDY > 0 ~ "edySDY",
                               total_SDY > 0 ~ "sdyOnly", 
                               total_EDY > 0 ~ "edyOnly")
   )
+
+awards %<>% 
+  mutate(sdy3_Awarded = SDY3s, 
+         sdy4_Awarded = SDY4s,
+         edy3_Awarded = CC_3s, 
+         edy4_Awarded = CC_4s
+         ) %>% 
+  mutate(total_SDY_Awarded = rowSums(select(., sdy3_Awarded, sdy4_Awarded), na.rm = TRUE),
+         total_EDY_Awarded = rowSums(select(., edy3_Awarded, edy4_Awarded), na.rm = TRUE),
+         awarded_Pgm_Model = case_when(total_SDY_Awarded > 0 & total_EDY_Awarded > 0 ~ "edySDY",
+                                       total_SDY_Awarded > 0 ~ "sdyOnly",
+                                       total_EDY_Awarded > 0 ~ "edyOnly"))
+
+#Data Joins & Manipulation ----
+
+combined <- left_join(awards, allSites, by = c("DoeSiteId" = "SiteID")) %>% 
+  mutate(total_CurrentSeats = rowSums(select(., total_SDY, total_EDY), na.rm = TRUE),
+         total_AwardSeats = rowSums(select(., total_SDY_Awarded, total_EDY_Awarded),
+                                    na.rm = TRUE), 
+         change_Pgm_Model = if_else(current_Pgm_Model == awarded_Pgm_Model, 
+                                    "model_Same", "model_Different")) %>% 
+  unite(., modelChange, current_Pgm_Model, awarded_Pgm_Model, sep = "->", remove = FALSE)
 
 
